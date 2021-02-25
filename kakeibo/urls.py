2704a8f91ab3d122026006c0f62d9523f7f6202f
doda999 <64188605+doda999@users.noreply.github.com>
2021-02-25 23:51:32 +0900

    admin画面のスタイルを変更

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth.models import Group
+
+admin.site.site_title = 'Kakeibo 内部管理サイト'
+admin.site.site_header = 'Kakeibo 内部管理サイト'
+admin.site.index_title = 'メニュー'
+admin.site.unregister(Group)
+admin.site.disable_action('delete_selected')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('staff-admin/', admin.site.urls),
     path('',include('accounts.urls')),
     path('money/', include('money.urls')),
 ]
